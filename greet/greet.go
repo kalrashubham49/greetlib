e809aa2d2fb6c492d38bb5bc70917b4a050328a5
Shubham <>
2020-04-25 16:22:34 +0530

    First Commit

---
+++ greet/greet.go
@@ -0,0 +1,15 @@
+package greet
+
+import "fmt"
+
+func GreetSomeone() {
+	fmt.Println("I said Hello!!")
+}
+
+func GreetRelative() {
+	fmt.Println("Hola!! , How are you doing mate ?")
+}
+
+func GreetMe() {
+	fmt.Println("Hey!!! Dont you know i am learning Golang..!!")
+}
